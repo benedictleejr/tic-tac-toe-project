f5074176c21bf9dfdec7e473a3b4d730b23e3ec9
benedictleejr <belasolee@gmail.com>
2025-06-06 11:54:02 +0800

    Add playername functionality and setup gameboard

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName || "Player");
+
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleNameChange = (event) => {
+    setPlayerName(event.target.value);
+  };
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{playerName}</span>
+        ) : (
+          <input
+            type="text"
+            required
+            defaultValue={playerName}
+            onChange={handleNameChange}
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
